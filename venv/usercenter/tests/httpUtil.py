4d70f2742181d59029c2c63aabe224d0b1dd622d
lijiamei <jiamei.li@ymm56.com>
2018-09-10 17:32:28 +0800

    将编译环境和依赖换成python2.7

--- venv/usercenter/tests/httpUtil.py
+++ venv/usercenter/tests/httpUtil.py
@@ -1,9 +1,13 @@
-import urllib.request
+#import urllib.request
+import urllib2
+import urllib
 import json
 
 def Post(url,headers,body):
-        req = urllib.request.Request(url=url, data=json.dumps(body).encode(), method="POST",headers=headers)
-        response = urllib.request.urlopen(req).read()
+        #req = urllib.request.Request(url=url, data=json.dumps(body).encode(), method="POST",headers=headers)
+        req = urllib2.Request(url=url, data=json.dumps(body).encode(),headers=headers)
+        #response = urllib.request.urlopen(req).read()
+        response = urllib2.urlopen(req).read()
         print(response.decode('utf8'))
         return response
 
@@ -11,8 +15,10 @@ def Post(url,headers,body):
 
 def Get(url,data,headers):
         new_url = url + "?" + "token="+data
-        req = urllib.request.Request(url = new_url,method="GET",headers=headers)
-        result = urllib.request.urlopen(req)
+        #req = urllib.request.Request(url = new_url,method="GET",headers=headers)
+        #result = urllib.request.urlopen(req)
+        req = urllib2.Request(url = new_url,headers=headers)
+        result = urllib2.urlopen(req)
         response = result.read()
         print(response.decode('utf8'))
         return response.decode('utf8')
