eed096e24ef39ef5b6f2619e2480c336c34a646c
lijiamei <jiamei.li@ymm56.com>
2018-09-13 18:09:16 +0800

    补充了用户中心的服务的case

--- venv/usercenter/tests/test_New_boss.py
+++ venv/usercenter/tests/test_New_boss.py
@@ -8,25 +8,23 @@ import time
 
 
 class new_boss_sharkTest(unittest.TestCase):
-    def test_new_boss(self):
+    def test_uc_doorkeeper_center(self):
         triggerUrl = data.trigger_url
         triggerHeaders = data.trigger_header
-        f = file('new_boss.json')
+        f = file('uc_doorkeeper_center.json')
         PostJson = json.load(f)
-
         triggerResponse = httpUtil.Post(triggerUrl, triggerHeaders, PostJson)
         triggerReponseJson = json.loads(triggerResponse)
-        timeData = triggerReponseJson['data']     #从触发器的接口中读出返回data，去查询对应的报告
-
-        time.sleep(6)
+        timeData = triggerReponseJson['data']  # 从触发器的接口中读出返回data，去查询对应的报告
+        time.sleep(20)
         reportUrl = data.report_url
         reportHeaders = data.report_header
         reportResponse = httpUtil.Get(reportUrl, timeData, reportHeaders)
         reporteponseJson = json.loads(reportResponse)
         total_run_failed = reporteponseJson['data']['summary']['total_run_failed']
         total_run_progress = reporteponseJson['data']['summary']['total_run_progress']
-        self.assertEqual(total_run_failed, 0, msg="执行失败")
-        self.assertEqual(total_run_progress, 0, msg="有正在执行的项目")
+        self.assertEqual(total_run_failed, 0, msg="uc_doorkeeper_center有执行失败的case")
+        self.assertEqual(total_run_progress, 0, msg="uc_doorkeeper_center有正在执行的项目")
 
 
 
--- venv/usercenter/tests/test_runAll.py
+++ venv/usercenter/tests/test_runAll.py
@@ -88,5 +88,99 @@ class runAll_sharkTest(unittest.TestCase):
         self.assertEqual(total_run_progress, 0, msg="ymm_info_app有正在执行的项目")
 
 
+
+    def test_ymm_reference_app(self):
+        triggerUrl = data.trigger_url
+        triggerHeaders = data.trigger_header
+        f = file('ymm_reference_app.json')
+        PostJson = json.load(f)
+        triggerResponse = httpUtil.Post(triggerUrl, triggerHeaders, PostJson)
+        triggerReponseJson = json.loads(triggerResponse)
+        timeData = triggerReponseJson['data']  # 从触发器的接口中读出返回data，去查询对应的报告
+        time.sleep(40)
+        reportUrl = data.report_url
+        reportHeaders = data.report_header
+        reportResponse = httpUtil.Get(reportUrl, timeData, reportHeaders)
+        reporteponseJson = json.loads(reportResponse)
+        total_run_failed = reporteponseJson['data']['summary']['total_run_failed']
+        total_run_progress = reporteponseJson['data']['summary']['total_run_progress']
+        self.assertEqual(total_run_failed, 0, msg="ymm_reference_app有执行失败的case")
+        self.assertEqual(total_run_progress, 0, msg="ymm_reference_app有正在执行的项目")
+
+    def test_authenticate_service(self):
+        triggerUrl = data.trigger_url
+        triggerHeaders = data.trigger_header
+        f = file('authenticate_service.json')
+        PostJson = json.load(f)
+        triggerResponse = httpUtil.Post(triggerUrl, triggerHeaders, PostJson)
+        triggerReponseJson = json.loads(triggerResponse)
+        timeData = triggerReponseJson['data']  # 从触发器的接口中读出返回data，去查询对应的报告
+        time.sleep(40)
+        reportUrl = data.report_url
+        reportHeaders = data.report_header
+        reportResponse = httpUtil.Get(reportUrl, timeData, reportHeaders)
+        reporteponseJson = json.loads(reportResponse)
+        total_run_failed = reporteponseJson['data']['summary']['total_run_failed']
+        total_run_progress = reporteponseJson['data']['summary']['total_run_progress']
+        self.assertEqual(total_run_failed, 0, msg="authenticate_service有执行失败的case")
+        self.assertEqual(total_run_progress, 0, msg="authenticate_service有正在执行的项目")
+
+
+    def test_ymm_userCenter_app(self):
+        triggerUrl = data.trigger_url
+        triggerHeaders = data.trigger_header
+        f = file('ymm_userCenter_app.json')
+        PostJson = json.load(f)
+        triggerResponse = httpUtil.Post(triggerUrl, triggerHeaders, PostJson)
+        triggerReponseJson = json.loads(triggerResponse)
+        timeData = triggerReponseJson['data']  # 从触发器的接口中读出返回data，去查询对应的报告
+        time.sleep(40)
+        reportUrl = data.report_url
+        reportHeaders = data.report_header
+        reportResponse = httpUtil.Get(reportUrl, timeData, reportHeaders)
+        reporteponseJson = json.loads(reportResponse)
+        total_run_failed = reporteponseJson['data']['summary']['total_run_failed']
+        total_run_progress = reporteponseJson['data']['summary']['total_run_progress']
+        self.assertEqual(total_run_failed, 0, msg="ymm_userCenter_app有执行失败的case")
+        self.assertEqual(total_run_progress, 0, msg="ymm_userCenter_app有正在执行的项目")
+
+    def test_uc_check_service(self):
+        triggerUrl = data.trigger_url
+        triggerHeaders = data.trigger_header
+        f = file('uc_check_service.json')
+        PostJson = json.load(f)
+        triggerResponse = httpUtil.Post(triggerUrl, triggerHeaders, PostJson)
+        triggerReponseJson = json.loads(triggerResponse)
+        timeData = triggerReponseJson['data']  # 从触发器的接口中读出返回data，去查询对应的报告
+        time.sleep(15)
+        reportUrl = data.report_url
+        reportHeaders = data.report_header
+        reportResponse = httpUtil.Get(reportUrl, timeData, reportHeaders)
+        reporteponseJson = json.loads(reportResponse)
+        total_run_failed = reporteponseJson['data']['summary']['total_run_failed']
+        total_run_progress = reporteponseJson['data']['summary']['total_run_progress']
+        self.assertEqual(total_run_failed, 0, msg="uc_check_service有执行失败的case")
+        self.assertEqual(total_run_progress, 0, msg="uc_check_service有正在执行的项目")
+
+
+    def test_uc_doorkeeper_center(self):
+        triggerUrl = data.trigger_url
+        triggerHeaders = data.trigger_header
+        f = file('uc_doorkeeper_center.json')
+        PostJson = json.load(f)
+        triggerResponse = httpUtil.Post(triggerUrl, triggerHeaders, PostJson)
+        triggerReponseJson = json.loads(triggerResponse)
+        timeData = triggerReponseJson['data']  # 从触发器的接口中读出返回data，去查询对应的报告
+        time.sleep(20)
+        reportUrl = data.report_url
+        reportHeaders = data.report_header
+        reportResponse = httpUtil.Get(reportUrl, timeData, reportHeaders)
+        reporteponseJson = json.loads(reportResponse)
+        total_run_failed = reporteponseJson['data']['summary']['total_run_failed']
+        total_run_progress = reporteponseJson['data']['summary']['total_run_progress']
+        self.assertEqual(total_run_failed, 0, msg="uc_doorkeeper_center有执行失败的case")
+        self.assertEqual(total_run_progress, 0, msg="uc_doorkeeper_center有正在执行的项目")
+
+
 if __name__ == '__main__':
     unittest.main()
