23f224201e39c812a64661fdd4ae45c285094bd8
lijiamei <jiamei.li@ymm56.com>
2018-09-13 16:47:09 +0800

    调整下时间

--- venv/usercenter/tests/test_runAll.py
+++ venv/usercenter/tests/test_runAll.py
@@ -26,8 +26,9 @@ class runAll_sharkTest(unittest.TestCase):
         reporteponseJson = json.loads(reportResponse)
         total_run_failed = reporteponseJson['data']['summary']['total_run_failed']
         total_run_progress = reporteponseJson['data']['summary']['total_run_progress']
-        self.assertEqual(total_run_failed, 0, msg="new_boss执行失败")
         self.assertEqual(total_run_progress, 0, msg="new_boss有正在执行的项目")
+        self.assertEqual(total_run_failed, 0, msg="new_boss执行失败")
+
 
     def test_userCenter(self):
         triggerUrl = data.trigger_url
@@ -44,8 +45,9 @@ class runAll_sharkTest(unittest.TestCase):
         reporteponseJson = json.loads(reportResponse)
         total_run_failed = reporteponseJson['data']['summary']['total_run_failed']
         total_run_progress = reporteponseJson['data']['summary']['total_run_progress']
-        self.assertEqual(total_run_progress, 0, msg="userCenter有正在执行的项目")
         self.assertEqual(total_run_failed, 0, msg="userCenter有执行失败的case")
+        self.assertEqual(total_run_progress, 0, msg="userCenter有正在执行的项目")
+
 
     def test_ymm_admin_app(self):
         triggerUrl = data.trigger_url
@@ -62,8 +64,9 @@ class runAll_sharkTest(unittest.TestCase):
         reporteponseJson = json.loads(reportResponse)
         total_run_failed = reporteponseJson['data']['summary']['total_run_failed']
         total_run_progress = reporteponseJson['data']['summary']['total_run_progress']
-        self.assertEqual(total_run_progress, 0, msg="ymm_admin_app有正在执行的项目")
         self.assertEqual(total_run_failed, 0, msg="ymm_admin_app有执行失败的case")
+        self.assertEqual(total_run_progress, 0, msg="ymm_admin_app有正在执行的项目")
+
 
 
     def test_ymm_info_app(self):
@@ -74,15 +77,16 @@ class runAll_sharkTest(unittest.TestCase):
         triggerResponse = httpUtil.Post(triggerUrl, triggerHeaders, PostJson)
         triggerReponseJson = json.loads(triggerResponse)
         timeData = triggerReponseJson['data']  # 从触发器的接口中读出返回data，去查询对应的报告
-        time.sleep(25)
+        time.sleep(40)
         reportUrl = data.report_url
         reportHeaders = data.report_header
         reportResponse = httpUtil.Get(reportUrl, timeData, reportHeaders)
         reporteponseJson = json.loads(reportResponse)
         total_run_failed = reporteponseJson['data']['summary']['total_run_failed']
         total_run_progress = reporteponseJson['data']['summary']['total_run_progress']
-        self.assertEqual(total_run_progress, 0, msg="ymm_info_app有正在执行的项目")
         self.assertEqual(total_run_failed, 0, msg="ymm_info_app有执行失败的case")
+        self.assertEqual(total_run_progress, 0, msg="ymm_info_app有正在执行的项目")
+
 
 if __name__ == '__main__':
     unittest.main()
