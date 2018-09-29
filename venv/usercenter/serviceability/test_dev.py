7f2c5ccf91a67481bae396ee21f5fa8c2d0ee9a8
lijiamei <jiamei.li@ymm56.com>
2018-09-29 19:34:42 +0800

    修复2.6不支持assert

--- venv/usercenter/serviceability/test_dev.py
+++ venv/usercenter/serviceability/test_dev.py
@@ -2,12 +2,12 @@
 # -*- coding:utf-8 -*-
 import sys
 import os.path
-import data.requestData
 curPath = os.path.abspath(os.path.dirname(__file__))
 rootPath = os.path.split(curPath)[0]
 sys.path.append(rootPath)
+import data.requestData
 import unittest
-import userCenter_app
+import base.userCenter_app
 
 
 
@@ -16,7 +16,7 @@ class runAll_Test(unittest.TestCase):
         env_url = data.requestData.dev
         path_base = os.path.abspath('..')  #获取上级目录
         path = path_base + "/data/getShipperInfo_dev.json"     #拼成绝对路径
-        responseJson= userCenter_app.getShipperInfo(env_url, path)
+        responseJson= base.userCenter_app.getShipperInfo(env_url, path)
         self.assertNotEqual(len(responseJson), 0, "dev环境userCenter_app可用性运行失败")
 
 if __name__ == '__main__':
