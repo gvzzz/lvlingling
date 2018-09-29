c2f76517d541a0592720b4cd5483e4eb6a86f7ae
lijiamei <jiamei.li@ymm56.com>
2018-09-29 19:30:42 +0800

    修复2.6不支持assert

--- venv/usercenter/serviceability/test_qa.py
+++ venv/usercenter/serviceability/test_qa.py
@@ -17,7 +17,7 @@ class runAll_Test(unittest.TestCase):
         path_base = os.path.abspath('..')  #获取上级目录
         path = path_base + "/data/getShipperInfo_qa.json"     #拼成绝对路径
         responseJson= base.userCenter_app.getShipperInfo(env_url, path)
-        self.assertIsNotNone(responseJson)
+        self.assertNotEqual(len(responseJson),0,"qa环境userCenter_app可用性运行失败")
 
 
 if __name__ == '__main__':
