a09907f708f4d618817764f8c1527c290f0fdd46
lijiamei <jiamei.li@ymm56.com>
2019-01-15 16:52:37 +0800

    可用性更新

--- venv/usercenter/tests/test_runAll.py
+++ venv/usercenter/tests/test_runAll.py
@@ -13,29 +13,18 @@ import base.admin_app
 
 class runAll_Test(unittest.TestCase):
 
-     #专门跑408的问题
-    def test_qa_408(self):
-         path_base = os.path.abspath('..')  #获取上级目录
-         path = path_base + "/data/getTelephoneAudit_qa.json"     #拼成绝对路径
-         env_url = "http://qa.ymmoa.com"
-         for i in range(10):
-             responseJson= base.admin_app.getTelephoneAudit(env_url, path)
-             self.assertNotEquals(len(responseJson),0,msg="跑408失败")
 
-
-
-
-    def test_All_UserCenter(self):
-        runGroupASync_postJson = data.requestData.runGroupASync_postJson
-        timeData = sharkUtil.runGroupASync(runGroupASync_postJson)
-        #timeData = "2018-11-22-19-28-58_19"
-        time.sleep(180)
-        reporteponseJson = sharkUtil.queryReport(timeData)
-        print  (reporteponseJson)
-        childrenArray =  reporteponseJson["data"]["summary"]["children"]
-        for i in range(len(childrenArray)):
-            self.assertEqual(childrenArray[i]["summary_info"]["total_failed"], 0,
-                             msg="有执行失败的case" + "报告地址：" + "http://shark.ymmoa.com/#/report/reportTable?report_type=project&ref_id=326&token=" + timeData)
+     def test_All_UserCenter(self):
+         runGroupASync_postJson = data.requestData.runGroupASync_postJson
+         timeData = sharkUtil.runGroupASync(runGroupASync_postJson)
+         #timeData = "2018-11-22-19-28-58_19"
+         time.sleep(180)
+         reporteponseJson = sharkUtil.queryReport(timeData)
+         print  (reporteponseJson)
+         childrenArray =  reporteponseJson["data"]["summary"]["children"]
+         for i in range(len(childrenArray)):
+             self.assertEqual(childrenArray[i]["summary_info"]["total_failed"], 0,
+                              msg="有执行失败的case" + "报告地址：" + "http://shark.ymmoa.com/#/report/reportTable?report_type=project&ref_id=326&token=" + timeData)
 
 
 
