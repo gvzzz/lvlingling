39d60f83575d59e94acc832833fd641f512f5d50
lijiamei <jiamei.li@ymm56.com>
2019-07-16 15:00:50 +0800

    更新下包的引入方式

--- venv/usercenter/serviceability/auto_registe_shipper_dev.py
+++ venv/usercenter/serviceability/auto_registe_shipper_dev.py
@@ -1,3 +1,8 @@
+import sys
+import os.path
+curPath = os.path.abspath(os.path.dirname(__file__))
+rootPath = os.path.split(curPath)[0]
+sys.path.append(rootPath)
 import base.userCenter_app
 import base.admin_app
 from tools import auto_create
